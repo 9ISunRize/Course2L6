0c978f9f9083c330a9be2c5ccc9ae58725dfbefb
Diana <dianamingazova162@gmail.com>
2023-11-30 09:26:49 +0500

    Выполнила ДЗ

---
+++ employeeAccountingBookkeeping/src/main/java/employeeAccountingAndBookkeeping/employeeAccountingBookkeeping/service/EmployeeServiceImpl.java
@@ -0,0 +1,52 @@
+package employeeAccountingAndBookkeeping.employeeAccountingBookkeeping.service;
+
+import employeeAccountingAndBookkeeping.employeeAccountingBookkeeping.expection.EmployeeAlreadyAddedException;
+import employeeAccountingAndBookkeeping.employeeAccountingBookkeeping.expection.EmployeeNotFoundException;
+import employeeAccountingAndBookkeeping.employeeAccountingBookkeeping.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final List<Employee> employees = new ArrayList<>();
+
+    @Override
+    public Employee add(String firstName, String secondName) {
+        Employee employee = new Employee(firstName, secondName);
+
+        if(employees.contains(employee)){
+            throw new EmployeeAlreadyAddedException();
+        }
+        employees.add(employee);
+        return employee;
+    }
+
+    @Override
+    public Employee remove(String firstName, String secondName) {
+        Employee employee = new Employee(firstName, secondName);
+        if(!employees.contains(employee)){
+            throw new EmployeeNotFoundException();
+        }
+        employees.remove(employee);
+        return employee;
+    }
+
+    @Override
+    public Employee find(String firstName, String secondName) {
+        Employee employee = new Employee(firstName, secondName);
+
+        if(employees.contains(employee)){
+            throw new EmployeeNotFoundException();
+        }
+        return employee;
+    }
+
+    @Override
+    public Collection<Employee> findAll() {
+        return employees ;
+    }
+}

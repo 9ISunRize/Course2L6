2822533cb2fc36a52797f20e77a12eb9a19fdce2
Diana <dianamingazova162@gmail.com>
2023-11-30 05:23:15 +0500

    Начальный коммит

---
+++ employeeAccountingBookkeeping/src/main/java/employeeAccountingAndBookkeeping/employeeAccountingBookkeeping/EmployeeAccountingBookkeepingApplication.java
@@ -0,0 +1,13 @@
+package employeeAccountingAndBookkeeping.employeeAccountingBookkeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAccountingBookkeepingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAccountingBookkeepingApplication.class, args);
+	}
+
+}
